e16f96bcdf5be56c0e85c249720ebf6f4e13ee6a
unknown <redcats_ftc@012-L019-ROBOT1.schools.MPSDS.edu>
2020-01-07 13:01:37 -0600

    First steps changing autonomous mode to use encoder and gyro

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonOpMode.java
@@ -14,7 +14,7 @@ public class AutonOpMode extends OpMode {
     DcMotor left_intake = null;
     DcMotor right_intake = null;
     Servo left_servo = null;
-
+    double encoder_start_l = 0.0;
     double left_trigger = 0.0;
     private ElapsedTime et = null;
     double elapsed_time = 0.0;
@@ -29,6 +29,7 @@ public class AutonOpMode extends OpMode {
         right_intake = hardwareMap.dcMotor.get("right_intake");
         left_servo = hardwareMap.servo.get("left_servo");
         et = new ElapsedTime();
+        encoder_start_l = left.get
 
         telemetry.addData("Status","running");
         telemetry.update();
@@ -39,28 +40,31 @@ public class AutonOpMode extends OpMode {
         elapsed_time = et.milliseconds() / 1000.0;
         telemetry.addData("Et (sec)", "%f", elapsed_time);
 
-        if (elapsed_time < 3.0) {
-            // go forward
-            right.setPower(power);
-            left.setPower(power);
-            telemetry.addData("Mode", "%s", "1 - Forward1");
+//        if (elapsed_time < 3.0) {
+//            // go forward
+//            right.setPower(power);
+//            left.setPower(power);
+//            telemetry.addData("Mode", "%s", "1 - Forward1");
+//
+//        } else if (elapsed_time < 5.25) {
+//            // turn left
+//            right.setPower(rotation_direction * power);
+//            left.setPower(-rotation_direction * power);
+//            telemetry.addData("Mode", "%s", "2 - Turn");
+//
+//        } else if (elapsed_time < 10.25) {
+//            //Go forward
+//            right.setPower(power);
+//            left.setPower(power);
+//            telemetry.addData("Mode", "%s", "3 - Forward");
+//
+//        } else {
+//            //halt
+//            right.setPower(0.0);
+//            left.setPower(0.0);
+//            telemetry.addData("Mode", "%s", "4 - Finished/Stopped");
+//        }
 
-        } else if (elapsed_time < 5.25) {
-            // turn left
-            right.setPower(rotation_direction * power);
-            left.setPower(-rotation_direction * power);
-            telemetry.addData("Mode", "%s", "2 - Turn");
 
-        } else if (elapsed_time < 10.25) {
-
-            right.setPower(power);
-            left.setPower(power);
-            telemetry.addData("Mode", "%s", "3 - Forward");
-
-        } else {
-            right.setPower(0.0);
-            left.setPower(0.0);
-            telemetry.addData("Mode", "%s", "4 - Finished/Stopped");
-        }
     }
 }
